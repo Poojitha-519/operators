5b3e400985a18a3838c1dbb2fd8718694c6b651b
Poojitha-519 <athmakurupoojitha@gmail.com>
2024-09-23 21:12:25 +0530

    Update Extras.py

--- Extras.py
+++ Extras.py
@@ -24,3 +24,15 @@ Sample input and output 2:
 1
 17
 '''
+
+def calculate_extras():
+    no_balls = int(input())
+    wides = int(input())
+    byes = int(input())
+    leg_byes = int(input())
+    penalties = int(input())
+     total_extras = no_balls + wides + byes + leg_byes + (penalties * 5)
+     print(total_extras)
+calculate_extras()
+
+
